fe8d5ff1844fe6819ea1605470412a22d6bddcfc
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-08 20:13:47 +0200

    Use product data to render products

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,8 +4,8 @@ import Button from '../Button/Button';
 import { useState } from 'react';
 
 const Product = props => {
-  const [currentColor, setCurrentColor] = useState('white');
-  const [currentSize, setCurrentSize] = useState('S');
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.sizes[0]);
 
   return (
     <article className={styles.product}>
@@ -18,24 +18,24 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <span className={styles.price}>Price: { props.basePrice }$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              <li><button type="button" className={styles.active}>{ props.sizes[0].name }</button></li>
+              <li><button type="button">{ props.sizes[1].name }</button></li>
+              <li><button type="button">{ props.sizes[2].name }</button></li>
+              <li><button type="button">{ props.sizes[3].name }</button></li>
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              <li><button type="button" className={clsx(props.colors[0] === 'blue' ? styles.colorBlue : styles.colorBlack, currentColor === props.colors[0] && styles.active)} /></li>
+              <li><button type="button" className={clsx(props.colors[1] === 'red' ? styles.colorRed : styles.colorRed, currentColor === props.colors[1] && styles.active)} /></li>
+              <li><button type="button" className={clsx(props.colors[2] === 'green' ? styles.colorGreen : styles.colorWhite, currentColor === props.colors[2] && styles.active)} /></li>
             </ul>
           </div>
           <Button className={styles.button}>
