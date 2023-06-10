f44051052aeddc5d37ae4126e3909f9135c688ba
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-10 12:42:41 +0200

    Create component OptionColor

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -1,6 +1,7 @@
 import styles from './ProductForm.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import OptionColor from '../OptionColor/OptionColor';
 
 const ProductForm = props => {
     return (
@@ -19,14 +20,7 @@ const ProductForm = props => {
                         <li><button type="button" className={clsx(props.currentSize.name === props.sizes[3].name && styles.active)} onClick={() => props.setCurrentSize(props.sizes[3])}>{props.sizes[3].name}</button></li>
                     </ul>
                 </div>
-                <div className={styles.colors}>
-                    <h3 className={styles.optionLabel}>Colors</h3>
-                    <ul className={styles.choices}>
-                        <li><button type="button" className={clsx(props.colors[0] === 'blue' ? styles.colorBlue : styles.colorWhite, props.currentColor === props.colors[0] && styles.active)} onClick={() => props.setCurrentColor(props.colors[0])} /></li>
-                        <li><button type="button" className={clsx(props.colors[1] === 'red' ? styles.colorRed : styles.colorBlack, props.currentColor === props.colors[1] && styles.active)} onClick={() => props.setCurrentColor(props.colors[1])} /></li>
-                        <li><button type="button" className={clsx(props.colors[2] === 'green' ? styles.colorGreen : styles.colorRed, props.currentColor === props.colors[2] && styles.active)} onClick={() => props.setCurrentColor(props.colors[2])} /></li>
-                    </ul>
-                </div>
+                <OptionColor colors={props.colors} currentColor={props.currentColor} setCurrentColor={props.setCurrentColor} />
                 <Button className={styles.button} action={event => props.action(event)}>
                     <span className="fa fa-shopping-cart" />
                 </Button>
